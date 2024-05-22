dd4b489125044be5ebfd1d4ed340ad2e0a7801c5
muhammad anwar <muhammadanwar.symits@gmail.com>
2024-05-22 15:13:52 +0500

    landing page almost finished

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <div className="background-image"></div>
       <div className="overlay"></div>
       <div className="content">
-        <Snowfall snowflakeCount={10} wind={[1, 0.5]} radius={[0.5, 1]} />
+        <Snowfall snowflakeCount={40} wind={[1, 0.5]} radius={[0.5, 1]} />
 
         <h1>Your Logo Here</h1>
         <LinkCards />
--- src/components/LinkCards/LinkCards.tsx
+++ src/components/LinkCards/LinkCards.tsx
@@ -1,29 +1,141 @@
 import { useState } from "react";
+import { SlClose } from "react-icons/sl";
+import discord from "../../assets/discord.png";
+import store from "../../assets/store.png";
+import voting from "../../assets/vote.png";
 import "./LinkCards.css"; // Create this CSS file if necessary
+
+interface ImageCard {
+  id: number;
+  title: string;
+  desc: string;
+  image: string;
+  link: string;
+}
 
 const LinkCards = () => {
   const [isSliderOpen, setIsSliderOpen] = useState(false);
 
-  const handleCardClick = () => {
-    setIsSliderOpen(true);
+  const handleCardClick = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    image: ImageCard
+  ) => {
+    e.stopPropagation();
+    if (image.link) {
+      window.open(image.link, "_blank");
+    } else {
+      setIsSliderOpen(true);
+    }
   };
 
   const handleCloseSlider = () => {
     setIsSliderOpen(false);
   };
 
+  const images: ImageCard[] = [
+    {
+      id: 1,
+      title: "Discord",
+      desc: "Chat on our",
+      image: discord,
+      link: "https://discord.com/invite/ePdJr3nra7",
+    },
+    {
+      id: 2,
+      title: "Store",
+      desc: "Donate on our",
+      image: store,
+      link: "https://store.skylyfe.net/",
+    },
+    {
+      id: 3,
+      title: "Voting",
+      desc: "Support us by",
+      image: voting,
+      link: "",
+    },
+    {
+      id: 4,
+      title: "Wiki",
+      desc: "Learn more on our",
+      image: discord,
+      link: "https://info.skylyfe.net/",
+    },
+  ];
+
+  const sliderLinks: ImageCard[] = [
+    {
+      id: 1,
+      title: "Discord",
+      desc: "Chat on our",
+      image: discord,
+      link: "https://discord.com/invite/ePdJr3nra7",
+    },
+    {
+      id: 2,
+      title: "Store",
+      desc: "Donate on our",
+      image: store,
+      link: "https://store.skylyfe.net/",
+    },
+    {
+      id: 3,
+      title: "Voting",
+      desc: "Support us by",
+      image: voting,
+      link: "",
+    },
+    {
+      id: 4,
+      title: "Wiki",
+      desc: "Learn more on our",
+      image: discord,
+      link: "https://info.skylyfe.net/",
+    },
+  ];
+
   return (
     <div className="link-cards-container">
       <div className="cards-container">
-        {[1, 2, 3, 4].map((card, index) => (
-          <div key={index} className="card" onClick={handleCardClick}>
-            Card {card}
+        {images.map((image) => (
+          <div
+            key={image.id}
+            className="card"
+            onClick={(e) => handleCardClick(e, image)}
+          >
+            <img
+              src={image.image}
+              alt={image.title}
+              className="card-image"
+              height={200}
+              width={200}
+            />
           </div>
         ))}
       </div>
       <div className={`slider ${isSliderOpen ? "open" : ""}`}>
-        <button onClick={handleCloseSlider}>X</button>
-        {/* <p>Slider Content Here</p> */}
+        <button onClick={handleCloseSlider}>
+          <SlClose />
+        </button>
+        <div className="slider-links">
+          {sliderLinks.map((link) => (
+            <div
+              key={link.id}
+              className="slider-link"
+              onClick={() => window.open(link.link, "_blank")}
+            >
+              <img
+                src={link.image}
+                alt={link.title}
+                className="slider-link-image"
+              />
+              <div className="slider-link-content">
+                <h3>{link.title}</h3>
+                <p>{link.desc}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
